b16620d6d51e5d583cfcab79028c6580e4d43b38
jordyo40 <jc.jordanchung@gmail.com>
2024-10-09 11:24:18 -0400

    commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+console.log("JavaScript file loaded correctly");
+
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function()){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('is-active');
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     const text = "Welcome";
     let index = 0;
@@ -12,4 +22,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     typeEffect();
+});
+
+// fade in about me
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Scroll event listener to apply the 'reveal' class when the element comes into view
+window.addEventListener('scroll', function() {
+    const aboutMeSection = document.querySelector('.aboutme__container');
+    if (aboutMeSection && isInViewport(aboutMeSection)) {
+        console.log("About Me section in view");
+
+        aboutMeSection.classList.add('reveal');
+    }
 });
